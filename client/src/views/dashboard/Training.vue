a1ad6d83727ff41e7d64b730222ba3fcc5eae0ed
artemij-petrovskij <petrovsky.artem.dev@gmail.com>
2020-10-28 16:27:31 +0000

    add exercises list

--- client/src/views/dashboard/Training.vue
+++ client/src/views/dashboard/Training.vue
@@ -1,65 +1,66 @@
 <template>
   <div class="training">
     <el-table :data="trainings" style="width: 100%">
-      <el-table-column width="80" prop="set" label="Подход"> </el-table-column>
       <el-table-column prop="exercise" label="Упражнение"> </el-table-column>
+      <el-table-column prop="set" label="Подход №"> </el-table-column>
 
-      <el-table-column width="80" prop="weight" label="Вес"> </el-table-column>
-      <el-table-column width="85" prop="repeats" label="Повторы"> </el-table-column>
+      <el-table-column prop="weight" label="Вес"> </el-table-column>
+      <el-table-column prop="repeats" label="Повторы"> </el-table-column>
     </el-table>
-    <el-form
-      style="
-        max-width: 400px;
-        position: relative;
-        margin: 0px auto;
-        padding: 20px;
-      "
-      :model="controls"
-      :rules="rules"
-      ref="controls"
-      label-position="left "
-    >
-      <el-form-item style="text-align: left">
-        Подход: {{ controls.set }}
-      </el-form-item>
-      <el-form-item label="Упражнение" prop="exercise">
-        <el-autocomplete
-          class="inline-input"
-          v-model="controls.exercise"
-          :fetch-suggestions="querySearch"
-          placeholder="Please Input"
-          required
-        ></el-autocomplete>
-      </el-form-item>
+    <div class="form">
+      <el-form
+        style="
+          max-width: 600px;
+          position: relative;
+          margin-left: 20px;
+          margin: 20px auto;
+          padding: 20px;
+        "
+        :model="controls"
+        :rules="rules"
+        ref="controls"
+        label-position="left "
+      >
+        <el-form-item class="set" style="text-align: left">
+          Подход: {{ controls.set }}
+        </el-form-item>
+        <el-form-item prop="exercise">
+          <el-autocomplete
+            v-model="controls.exercise"
+            :fetch-suggestions="querySearch"
+            placeholder="Please Input"
+          ></el-autocomplete>
+        </el-form-item>
 
-      <el-form-item label="Вес:">
-        <div class="block">
-          <span class="demonstration">{{ controls.weight }}</span>
-          <el-slider
-            v-model="controls.weight"
-            :show-tooltip="false"
-          ></el-slider>
-        </div>
-      </el-form-item>
-      <el-form-item label="Повторения:">
-        <div class="block">
-          <span class="demonstration">{{ controls.repeats }}</span>
-          <el-slider
-            v-model="controls.repeats"
-            :show-tooltip="false"
-          ></el-slider>
-        </div>
-      </el-form-item>
+        <el-form-item class="weight" label="Вес:">
+          <div class="block">
+            <span class="demonstration">{{ controls.weight }}</span>
+            <el-slider
+              v-model="controls.weight"
+              :show-tooltip="false"
+            ></el-slider>
+          </div>
+        </el-form-item>
+        <el-form-item class="repeats" label="Повторения:">
+          <div class="block">
+            <span class="demonstration">{{ controls.repeats }}</span>
+            <el-slider
+              v-model="controls.repeats"
+              :show-tooltip="false"
+            ></el-slider>
+          </div>
+        </el-form-item>
 
-      <el-button-group>
-        <el-button type="primary" @click="nextExercise('controls')"
-          >Следующее Упражнение</el-button
-        >
-        <el-button type="primary" @click="nextSet('controls')"
-          >Следующий подход <i class="el-icon-refresh"></i
-        ></el-button>
-      </el-button-group>
-    </el-form>
+        <el-button-group>
+          <el-button type="primary" @click="nextExercise('controls')"
+            >Закончить
+          </el-button>
+          <el-button type="primary" @click="nextSet('controls')"
+            >Подход <i class="el-icon-refresh"></i
+          ></el-button>
+        </el-button-group>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -79,7 +80,7 @@ export default {
         exercise: [
           {
             min: 1,
-            max: 40,
+            max: 80,
             required: true,
             message: "Введите упражнение",
             trigger: "change",
@@ -106,12 +107,36 @@ export default {
     },
     loadAll() {
       return [
-        { value: "element" },
-        { value: "cooking" },
-        { value: "mint-ui" },
-        { value: "vuex" },
-        { value: "vue-router" },
-        { value: "babel" },
+        { value: "Жим штанги лежа на горизонтальной скамье" },
+        { value: "Жим штанги лежа на наклонной скамье" },
+
+        { value: "Жим гантелей лежа на горизонтальной скамье" },
+        { value: "Жим гантелей лежа на наклонной скамье" },
+
+        { value: "Разведения гантелей лежа на наклонной скамье" },
+        { value: "Разведения гантелей лежа на горизонтальной скамье" },
+
+        { value: "Жим штанги лежа узким хватом на горизонтальной скамье" },
+        { value: "Жим штанги лежа узким хватом на наклонной скамье" },
+
+        { value: "Жим штанги сидя (смит)" },
+        { value: "Жим штанги стоя" },
+        { value: "Жим гантелей стоя" },
+
+        { value: "Подтягивания" },
+        { value: "Подтягивания обратным хватом" },
+
+        { value: "Подъемы гантелей (махи) через стороны вверх" },
+        { value: "Подъемы гантелей через стороны вверх в наклоне" },
+        { value: "Дэнчик (плечи)" },
+
+        { value: "Приседания со штангой на спине" },
+        { value: "Выпады с гантелями" },
+        { value: "Разгибание ног в тренажере" },
+        { value: "Жим ногами" },
+        { value: "Подъемы на носки сидя " },
+
+        { value: "Гиперэкстензии" },
       ];
     },
     nextSet(formName) {
@@ -158,6 +183,8 @@ export default {
 <style lang="css" scoped>
 .el-form {
   text-align: right !important;
+  background-color: #dbd9de;
+  border-radius: 20px;
 }
 .el-button-group {
   width: 100%;
@@ -165,8 +192,17 @@ export default {
 .el-button {
   width: 50%;
 }
-.el-form-item__error::after {
-  position: relative !important;
-  right: 0;
+
+.form {
+  padding: 10px;
+}
+.set,
+.weight,
+.repeats {
+  font-weight: bold;
+  color: black !important;
+}
+.el-autocomplete {
+  width: 100%;
 }
 </style>
